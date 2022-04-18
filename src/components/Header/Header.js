10c38cbd1eaa44afb30a953bf00795f49e63fad8
tarun <tarun@appinessworld.com>
2022-04-18 11:38:01 +0530

    five

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import "./Header.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+
+  function navLinkStyle ({isActive}) {
+    return{
+      fontWeight : isActive ? "bold" : ""
+    }
+  }
+
   return (
     <div className='header'>
       <h1 className='heading'>Hacker News Clone</h1>
       <ul className='nav-links'>
-        <li className='nav-link'><Link to='/top' className='link'>TOP STORIES</Link></li>
-        <li className='nav-link'><Link to='/new' className='link'>LATEST STORIES</Link></li>
-        <li className='nav-link'><Link to='/best' className='link'>BEST STORIES</Link></li>
+        <li className='nav-link'><NavLink to='/top' style={navLinkStyle} className="link">TOP STORIES</NavLink></li>
+        <li className='nav-link'><NavLink to='/new' style={navLinkStyle} className="link">LATEST STORIES</NavLink></li>
+        <li className='nav-link'><NavLink to='/best' style={navLinkStyle} className="link">BEST STORIES</NavLink></li>
       </ul>
     </div>
   )
